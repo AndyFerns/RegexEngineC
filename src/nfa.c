22308985c6b65cdb3758347fa231edace8bc3ad4
AndyFerns <write2andrew.fernandes@gmail.com>
2025-10-26 05:26:57 +0530

    added placeholder values for future implementation

---
+++ src/nfa.c
@@ -0,0 +1,25 @@
+#include "nfa.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+// Leaving stub functions for future implementations
+
+Nfa* create_nfa_for_char(char c) {
+    // TODO: Implement this
+    return NULL;
+}
+
+Nfa* create_nfa_for_concat(Nfa* nfa1, Nfa* nfa2) {
+    // TODO: Implement this
+    return NULL;
+}
+
+Nfa* create_nfa_for_union(Nfa* nfa1, Nfa* nfa2) {
+    // TODO: Implement this
+    return NULL;
+}
+
+Nfa* create_nfa_for_star(Nfa* nfa) {
+    // TODO: Implement this
+    return NULL;
+}

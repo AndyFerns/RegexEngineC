73d0baa272d7bbbd883b9670393ca6adec5de97a
AndyFerns <write2andrew.fernandes@gmail.com>
2025-11-17 04:53:16 +0530

    added simulate_nfa functionality + state-tracking logic

--- include/simulator.h
+++ include/simulator.h
@@ -1,7 +1,17 @@
 #ifndef SIMULATOR_H
 #define SIMULATOR_H
 
-// TODO
-// Functions for simulation will go here later
+#include "nfa.h"
+
+/**
+ * @brief Simulates an NFA against a given string
+ * 
+ * @param nfa The NFA (Start State) to emulate
+ * 
+ * @returns 1 (true) if the string is accepted
+ * 
+ * @returns 0 (false) if the string is rejected
+ */
+int simulate_nfa(Nfa* nfa, const char* str);
 
 #endif
